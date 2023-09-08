22f4f75d6a3b0013b3a9a2e395886ce41cb11286
Eugene Cross <eugene.cross@gmail.com>
2023-09-08 10:29:53 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Eugene on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
