5061bc764571a1303baae5adc00b56e6a90a1a72
Eugene Cross <eugene.cross@gmail.com>
2023-09-08 13:10:40 +0100

    Day 70, improving annotations on map

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -19,7 +19,19 @@ struct ContentView: View {
         ZStack {
             Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
                 
-                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                MapAnnotation(coordinate: location.coordinate) {
+                    VStack {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .frame(width: 44, height: 44) // minimum size for interactive element Apple HIG
+                            .background(.white)
+                            .clipShape(Circle())
+                        
+                        Text(location.name)
+                    }
+                    
+                }
             }
                 .ignoresSafeArea()
             
