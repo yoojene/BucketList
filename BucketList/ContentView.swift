f1f1792fdf9c91a5c082eb33e0578d8dbee9800e
Eugene Cross <eugene.cross@gmail.com>
2023-09-08 11:44:02 +0100

    Day 69: Integrating MapKit in SwiftUI

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,16 +5,49 @@
 //  Created by Eugene on 08/09/2023.
 //
 
+import MapKit
 import SwiftUI
+
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
 
 struct ContentView: View {
     
+    @State private var mapRegion = MKCoordinateRegion(center:
+        CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span:
+        MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
     var body: some View {
-        Text("Hello")
-            .onTapGesture {
-                let message = FileManager.default.decode("Test message to bundle", "Messagesss.txt")
-                print(message)
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+    //            MapMarker(coordinate: location.coordinate)
+                
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label : {
+                        VStack {
+                            Circle()
+                                .stroke(.red, lineWidth: 3)
+                                .frame(width: 44, height: 44)
+//                                .onTapGesture {
+//                                    print("Tapped on \(location.name)")
+//                                }
+//                            Text(location.name)
+                        }
+                    }
+                }
             }
+        }
+
     }
 }
 
