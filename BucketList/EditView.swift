8ef363ff918466f1987ad36904482c98aaee0e24
Eugene Cross <eugene.cross@gmail.com>
2023-09-08 23:10:19 +0100

    Day 71: downloading data from wikipedia

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct EditView: View {
     
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+    
     @Environment(\.dismiss) var dismiss
     
     var location: Location
@@ -17,6 +21,9 @@ struct EditView: View {
     @State private var name: String
     @State private var description: String
     
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
+    
     var body: some View {
         NavigationView {
             Form {
@@ -24,6 +31,25 @@ struct EditView: View {
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
                 }
+                
+                Section("Nearby..") {
+                    switch loadingState {
+                    case .loading:
+                        Text("Loading")
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            /*@START_MENU_TOKEN@*/Text(page.title)/*@END_MENU_TOKEN@*/
+                                .font(.headline)
+                            
+                            + Text(": ")
+                            + Text("Page description here")
+                                .italic()
+                        }
+                    case .failed:
+                        Text("Please try again later.")
+                    }
+                }
+                
             }
             .navigationTitle("Place details")
             .toolbar {
@@ -36,6 +62,9 @@ struct EditView: View {
                     onSave(newLocation)
                     dismiss()
                 }
+                .task {
+                    await fetchNearbyPlaces()
+                }
             }
         }
     }
@@ -47,6 +76,24 @@ struct EditView: View {
         // _<var> will update the State struct value of each property wrapper
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
+    }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            loadingState = .loaded
+        } catch {
+            loadingState = .failed
+        }
     }
 }
 
---
+++ Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  BucketList
+//
+//  Created by Eugene on 08/09/2023.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
