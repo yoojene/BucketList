906f1d4a48c065e83520d8ef69cabb1b801bee40
Eugene Cross <eugene.cross@gmail.com>
2023-09-08 11:57:09 +0100

    Day 69: FaceID / TouchID

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,50 +5,49 @@
 //  Created by Eugene on 08/09/2023.
 //
 
-import MapKit
+import LocalAuthentication
 import SwiftUI
-
-struct Location: Identifiable {
-    let id = UUID()
-    let name: String
-    let coordinate: CLLocationCoordinate2D
-}
 
 struct ContentView: View {
     
-    @State private var mapRegion = MKCoordinateRegion(center:
-        CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span:
-        MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
-    
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
+    @State private var isUnlocked = false
     
     var body: some View {
-        NavigationView {
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-    //            MapMarker(coordinate: location.coordinate)
-                
-                MapAnnotation(coordinate: location.coordinate) {
-                    NavigationLink {
-                        Text(location.name)
-                    } label : {
-                        VStack {
-                            Circle()
-                                .stroke(.red, lineWidth: 3)
-                                .frame(width: 44, height: 44)
-//                                .onTapGesture {
-//                                    print("Tapped on \(location.name)")
-//                                }
-//                            Text(location.name)
-                        }
-                    }
-                }
+        VStack {
+            if isUnlocked {
+                Text("unlocked!")
+            } else {
+                Text("locked")
             }
         }
-
+        .onAppear(perform: authenticate)
     }
+    
+    func authenticate() {
+        
+        let context = LAContext()
+        
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            let reason = "We need to unlock your data" // FaceID reason
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a prob
+                }
+            }
+            
+        } else {
+            // no biometrics
+        }
+        
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
