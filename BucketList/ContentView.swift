678d11b9857208587155ea25d6930a72aa02aa15
Eugene Cross <eugene.cross@gmail.com>
2023-09-08 11:13:15 +0100

    Day 68, add data to docs directory

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello")
+            .onTapGesture {
+                let str = "Test message"
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+                
+            }
+    }
+    
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask) // sandboxed directory for the app
+        return paths[0]
     }
 }
 
